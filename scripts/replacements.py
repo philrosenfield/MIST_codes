fa3e26652a7d9b15c5e658c5f6f5997dd4b6e866
Phil Rosenfield <philrosenfield@gmail.com>
2018-03-26 14:48:00 -0400

    added some documentation

--- scripts/replacements.py
+++ scripts/replacements.py
@@ -1,6 +1,7 @@
-lowinter = {'amlt' = 1.82,
-            'asc' = 0.1,
-            'fovc' = 0.016,
-            'fovc0' = 0.008,
-            'ath' = 666,
-            'etar' = 0.1}
+# Values from Choi+2016
+lowinter = {'amlt': 1.82,  # alpha MLT
+            'asc': 0.1,  # alpha Semiconvection
+            'fovc': 0.016, # core overshooting strength
+            'fovc0': 0.008,  # initial core overshooting strength (fovc / 2)
+            'ath': 666.,  # alpha Thermohaline
+            'etar': 0.1}  # eta Reimers
