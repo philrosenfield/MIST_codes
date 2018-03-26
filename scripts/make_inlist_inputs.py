b27b00780fbf8d22b6ee3c6951662821346f5c65
Phil Rosenfield <philrosenfield@gmail.com>
2018-03-26 13:00:07 -0400

    Revert "some pretty-ing mostly adding more template values to inlist_lowinter."
    This reverts commit a15650a5d48bd8d279ea9aae9cbb67baf0fdc8c6.

--- scripts/make_inlist_inputs.py
+++ scripts/make_inlist_inputs.py
@@ -22,23 +22,19 @@ Example:
     >>> replist = make_inlist_inputs(runname, 'VeryLow')
 
 Acknowledgment:
-    Thanks to Joshua Burkart for providing assistance with and content for
-    earlier versions of this code.
+    Thanks to Joshua Burkart for providing assistance with and content for earlier versions of this code.
 
 """
 
 import sys
-
 import numpy as np
-
 
 def make_inlist_inputs(runname, startype, feh, afe, zbase, rot, net,
                        amlt=1.82, asc=0.1, fovc=0.016, fovc0=0.008,
                        ath=666, etar=0.1):
 
-    # Array of all masses
-    def massgrid(i, f, step): return np.linspace(
-        i, f, round(((f - i) / step)) + 1.0)
+    #Array of all masses
+    massgrid = lambda i,f,step: np.linspace(i,f,round(((f-i)/step))+1.0)
 
 #    bigmassgrid = np.unique(np.hstack(massgrid(0.6,2.0,0.1)))
 #    bigmassgrid = np.unique(np.hstack(massgrid(2.1,3.0,0.1)))
@@ -48,7 +44,7 @@ def make_inlist_inputs(runname, startype, feh, afe, zbase, rot, net,
                                        massgrid(2.0, 12.0, 0.5),
                                        massgrid(12.0, 40.0, 2))))
 
-    # Choose the correct mass range and boundary conditions
+    #Choose the correct mass range and boundary conditions
     if (startype == 'VeryLow'):
         massindex = np.where(bigmassgrid < 0.30)
         bctype = 'tau_100_tables'
@@ -77,89 +73,87 @@ def make_inlist_inputs(runname, startype, feh, afe, zbase, rot, net,
         print 'Invalid choice.'
         sys.exit(0)
 
-    # Create mass lists
-    def mapfunc(var): return np.str(
-        int(var)) if var == int(var) else np.str(var)
+    #Create mass lists
+    mapfunc = lambda var: np.str(int(var)) if var == int(var) else np.str(var)
     masslist = map(mapfunc, bigmassgrid[massindex])
 
-    # Create BC lists
+    #Create BC lists
     if ('Diff' in startype):
-        bctablelist = list([bctype1] * np.size(massindex)) + \
-            list([bctype2] * np.size(massindex))
-        bclabellist = list([bclabel1] * np.size(massindex)) + \
-            list([bclabel2] * np.size(massindex))
+        bctablelist = list([bctype1]*np.size(massindex))+list([bctype2]*np.size(massindex))
+        bclabellist = list([bclabel1]*np.size(massindex))+list([bclabel2]*np.size(massindex))
     else:
-        bctablelist = list([bctype] * np.size(massindex))
-        bclabellist = list([bclabel] * np.size(massindex))
+        bctablelist = list([bctype]*np.size(massindex))
+        bclabellist = list([bclabel]*np.size(massindex))
 
-    # Create [a/Fe] lists
-    afelist = list([afe] * np.size(massindex))
+    #Create [a/Fe] lists
+    afelist = list([afe]*np.size(massindex))
 
-    # create [Fe/H] lists
-    fehlist = list([feh] * np.size(massindex))
+    #create [Fe/H] lists
+    fehlist = list([feh]*np.size(massindex))
 
-    # Create Zbase list
-    zbaselist = list([zbase] * np.size(massindex))
+    #Create Zbase list
+    zbaselist = list([zbase]*np.size(massindex))
 
-    # Create rot list
-    rotlist = list([rot] * np.size(massindex))
+    #Create rot list
+    rotlist = list([rot]*np.size(massindex))
 
-    # Create net list
-    netlist = list([net] * np.size(massindex))
+    #Create net list
+    netlist = list([net]*np.size(massindex))
 
-    # Create alpha mlt list
-    amltlist = list([amlt] * np.size(massindex))
+    #Create alpha mlt list
+    amltlist = list([amlt]*np.size(massindex))
 
     # Create alpha semiconvection list
-    asclist = list([asc] * np.size(massindex))
+    asclist = list([asc]*np.size(massindex))
 
     # Create core overshoot strength list
-    fovclist = list([fovc] * np.size(massindex))
+    fovclist = list([fovc]*np.size(massindex))
 
     # Create core overshoot strength 0 list (typically fovc/2)
-    fovc0list = list([fovc0] * np.size(massindex))
+    fovc0list = list([fovc0]*np.size(massindex))
 
     # Create alpha thermohaline list
-    athlist = list([ath] * np.size(massindex))
+    athlist = list([ath]*np.size(massindex))
 
     # Create Reimers eta list
-    etarlist = list([etar] * np.size(massindex))
+    etarlist = list([etar]*np.size(massindex))
 
-    # Make list of [replacement string, values]
-    replist = [
-        ["<<MASS>>", masslist],
-        ["<<BC_LABEL>>", bclabellist],
-        ["<<BC_TABLE>>", bctablelist],
-        ["<<AFE>>", afelist],
-        ["<<FEH>>", fehlist],
-        ["<<ZBASE>>", zbaselist],
-        ["<<ROT>>", rotlist],
-        ["<<NET>>", netlist],
-        ["<<AMLT>>", amltlist],
-        ["<<ASC>>", asclist],
-        ["<<FOVC>>", fovclist],
-        ["<<FOVC0>>", fovc0list],
-        ["<<ATH>>", athlist],
-        ["<<ETAR>>", etarlist],
-    ]
-
-    # Special case for LowDiffBC
-    if ('Diff' in startype):
-        replist = [
-            ["<<MASS>>", masslist * 2],
-            ["<<BC_LABEL>>", bclabellist],
-            ["<<BC_TABLE>>", bctablelist],
-            ["<<AFE>>", afelist * 2],
-            ["<<FEH>>", fehlist * 2],
-            ["<<ZBASE>>", zbaselist * 2],
-            ["<<ROT>>", rotlist * 2],
-            ["<<NET>>", netlist * 2],
-            ["<<AMLT>>", amltlist * 2],
-            ["<<ASC>>", asclist * 2],
-            ["<<FOVC>>", fovclist * 2],
-            ["<<FOVC0>>", fovc0list * 2],
-            ["<<ATH>>", athlist * 2],
-            ["<<ETAR>>", etarlist * 2],
+    #Make list of [replacement string, values]
+    replist = [\
+            ["<<MASS>>", masslist],\
+            ["<<BC_LABEL>>", bclabellist],\
+            ["<<BC_TABLE>>", bctablelist],\
+            ["<<AFE>>", afelist],\
+            ["<<FEH>>", fehlist],\
+            ["<<ZBASE>>", zbaselist],\
+            ["<<ROT>>", rotlist],\
+            ["<<NET>>", netlist],\
+            ["<<AMLT>>", amltlist],\
+            ["<<ASC>>", asclist],\
+            ["<<FOVC>>", fovclist],\
+            ["<<FOVC0>>", fovc0list],\
+            ["<<ATH>>", athlist],\
+            ["<<ETAR>>", etarlist],\
         ]
 
+    #Special case for LowDiffBC
+    if ('Diff' in startype):
+        replist = [\
+                ["<<MASS>>", masslist*2],\
+                ["<<BC_LABEL>>", bclabellist],\
+                ["<<BC_TABLE>>", bctablelist],\
+                ["<<AFE>>", afelist*2],\
+                ["<<FEH>>", fehlist*2],\
+                ["<<ZBASE>>", zbaselist*2],\
+                ["<<ROT>>", rotlist*2],\
+                ["<<NET>>", netlist*2],\
+                ["<<AMLT>>", amltlist*2],\
+                ["<<ASC>>", asclist*2],\
+                ["<<FOVC>>", fovclist*2],\
+                ["<<FOVC0>>", fovc0list*2],\
+                ["<<ATH>>", athlist*2],\
+                ["<<ETAR>>", etarlist*2],\
+            ]
+
     return replist
+
--- scripts/replacements.py
+++
@@ -1,6 +0,0 @@
-lowinter = {'amlt' = 1.82,
-            'asc' = 0.1,
-            'fovc' = 0.016,
-            'fovc0' = 0.008,
-            'ath' = 666,
-            'etar' = 0.1}
